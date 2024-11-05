ad1ce28db17d6962680e2d6b445a421eec777de8
Ivan Sapozhnikov <ivan101454@gmail.com>
2024-11-06 00:10:38 +0300

    add interceptor class

--- src/main/java/ru/clevertec/travelguide/service/GuideService.java
+++ src/main/java/ru/clevertec/travelguide/service/GuideService.java
@@ -1,5 +1,6 @@
 package ru.clevertec.travelguide.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import ru.clevertec.travelguide.dao.Repository;
 import ru.clevertec.travelguide.dto.GuideDto;
@@ -14,26 +15,31 @@ public class GuideService implements Service<Guide, GuideDto, Long> {
 
     private final Repository<Long, Guide> guideRepository;
 
+    @Transactional
     @Override
     public Optional<GuideDto> findById(Long id) {
         return guideRepository.finById(id).map(MapperGuide.INSTANCE::guideToGuideDto);
     }
 
+    @Transactional
     @Override
     public List<GuideDto> findAllAuthor() {
         return guideRepository.findAll().stream().map(MapperGuide.INSTANCE::guideToGuideDto).toList();
     }
 
+    @Transactional
     @Override
     public Guide create(GuideDto dto) {
         return guideRepository.save(MapperGuide.INSTANCE.guideDtoToGuide(dto));
     }
 
+    @Transactional
     @Override
     public void update(GuideDto dto) {
         guideRepository.update(MapperGuide.INSTANCE.guideDtoToGuide(dto));
     }
 
+    @Transactional
     @Override
     public boolean delete(Long id) {
         Optional<Guide> guide = guideRepository.finById(id);
--- src/test/java/ru/clevertec/travelguide/dao/integration/GuideServiceIT.java
+++ src/test/java/ru/clevertec/travelguide/dao/integration/GuideServiceIT.java
@@ -1,5 +1,8 @@
 package ru.clevertec.travelguide.dao.integration;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.jupiter.api.AfterAll;
@@ -8,10 +11,13 @@ import org.junit.jupiter.api.Test;
 import ru.clevertec.travelguide.dao.AuthorRepository;
 import ru.clevertec.travelguide.dao.GuideRepository;
 import ru.clevertec.travelguide.dto.GuideDto;
+import ru.clevertec.travelguide.interceptor.TransactionInterceptor;
 import ru.clevertec.travelguide.service.AuthorService;
 import ru.clevertec.travelguide.service.GuideService;
+import ru.clevertec.travelguide.service.UserService;
 import ru.clevertec.travelguide.utils.HibernateUtil;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Proxy;
 import java.util.Optional;
 
@@ -23,21 +29,37 @@ public class GuideServiceIT {
     static GuideService guideService;
 
     @BeforeAll
-    static void initDatabaseSession() {
+    static void initDatabaseSession() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         sessionFactory = HibernateUtil.buildSessionFactory();
         session = (Session) Proxy.newProxyInstance(SessionFactory.class.getClassLoader(), new Class[]{Session.class},
                 ((proxy, method, args) -> method.invoke(sessionFactory.getCurrentSession(), args)));
         guideRepository = new GuideRepository(session);
-        guideService = new GuideService(guideRepository);
+        var guideService1 = new GuideService(guideRepository);
+
+        TransactionInterceptor transactionInterceptor = new TransactionInterceptor(sessionFactory);
+        guideService = new ByteBuddy()
+                .subclass(GuideService.class)
+                .method(ElementMatchers.any())
+                .intercept(MethodDelegation.to(transactionInterceptor))
+                .make()
+                .load(UserService.class.getClassLoader())
+                .getLoaded()
+                .getDeclaredConstructor(GuideRepository.class)
+                .newInstance(guideRepository);
     }
 
     @Test
     void shouldReturnListOfCharter() {
-        session.beginTransaction();
+//        session.beginTransaction();
         Optional<GuideDto> biId = guideService.findById(1L);
         biId.get().getContentsCharter().forEach(System.out::println);
-        session.getTransaction().commit();
+//        session.getTransaction().commit();
     }
+
+    void shouldCreateNewGuide() {
+
+    }
+
 
     @AfterAll
     static void closeDatabase() {
